2ae7e9a9ac663387461dd45d5137d1b348b3566a
Your Name <you@example.com>
2015-08-23 12:45:37 +0600

    init

---
+++ newpro/src/y/p/newpro/NewproActivity.java
@@ -0,0 +1,13 @@
+package y.p.newpro;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewproActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
